0651fa495a53cc64dbb2e48367d089208b298979
Garrett Kelly Stocksdale <gstocksd@purdue.edu>
2017-06-04 11:30:40 -0400

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Garrett Kelly Stocksdale on 6/4/17.
+//  Copyright © 2017 Garrett Kelly Stocksdale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
